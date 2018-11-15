8df66512037f8dd1b87e8c4efda073b6381d0b3a
JosephStar318 <44904505+JosephStar318@users.noreply.github.com>
2018-11-15 21:32:23 +0300

    Update odev1.c

--- odev1.c
+++ odev1.c
@@ -1,20 +1,22 @@
-  #include <stdio.h>
-  int main(){
+ #include <stdio.h>
+  int main(void){
   
   int a,b,c,sonuc;
-  printf("Yapmak istediğiniz işlemi seçin. Toplamak için 1, çıkarmak için 2, çarpmak için 3, bölmek için 4, mod almak için 5 yazın.");
+  printf("Yapmak istediginiz islemi secin. Toplamak icin 1, cikarmak icin 2, carpmak icin 3, bolmek icin 4, mod almak icin 5 yazin.\n");
   scanf("%d",&a);
   
-  printf("2 adet sayı girin.");
+  printf("2 adet sayi girin.\n");
   scanf("%d %d",&b,&c);
+  printf("Bu sayilari girdiniz %d %d\n",b,c);
+  if      (a==1) sonuc=(b+c);
+  else if (a==2) sonuc=(b-c);
+  else if (a==3) sonuc=(b*c);
+  else if (a==4) sonuc=(b/c);
+  else if (a==5) sonuc=(b%c);
+
   
-  if      (a==1) sonuc=b+c;
-  else if (a==2) sonuc=b-c;
-  else if (a==3) sonuc=b*c;
-  else if (a==4) sonuc=b/c;
-  else if (a==5) sonuc=b%c;
-  
-  printf("Sonuç %d",&sonuc);
+  printf("Sonuç %d\n",sonuc);
     
-    return 0;
+    getch();
+    main();
 }
